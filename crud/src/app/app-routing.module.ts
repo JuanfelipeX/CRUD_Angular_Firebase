1b9a9dd32348d21baa985fc8531ae6faa0d8abec
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-10 15:38:34 -0500

    ng g m pages/employees/details --module app --route details

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/employees/list/list.module').then(m => m.ListModule) }, { path: 'new', loadChildren: () => import('./pages/employees/new/new.module').then(m => m.NewModule) }];
+const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/employees/list/list.module').then(m => m.ListModule) }, { path: 'new', loadChildren: () => import('./pages/employees/new/new.module').then(m => m.NewModule) }, { path: 'details', loadChildren: () => import('./pages/employees/details/details.module').then(m => m.DetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
