f09d62ccb365daed00060cea60a91760062ac439
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-13 16:01:14 -0500

    agregating methods  for onGoToEdit and onGoBackToList

--- crud/src/app/pages/employees/details/details.component.ts
+++ crud/src/app/pages/employees/details/details.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  navigationExtras: NavigationExtras = {
+    state: {
+      value: null,
+    },
+  };
 
-  constructor() { }
+  employee: any = null;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.employee = navigation?.extras?.state;
   }
 
+  ngOnInit(): void {}
+
+  onGoToEdit(): void {
+    this.navigationExtras!.state!['value'] = this.employee;
+    this.router.navigate(['edit'], this.navigationExtras);
+  }
+
+  onGoBackToList(): void {
+    this.router.navigate(['list']);
+  }
 }
--- crud/src/app/pages/employees/edit/edit.component.ts
+++ crud/src/app/pages/employees/edit/edit.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
+  employee: any = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.employee = navigation?.extras?.state;
   }
 
+  ngOnInit(): void {}
 }
