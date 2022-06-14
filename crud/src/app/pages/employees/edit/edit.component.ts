32d1346ecef5684322d4b38d7f13e12aff7aaaf1
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-14 11:57:06 -0500

    deleting the logic EditComponent because where are going to use this logic in employee-form.ts

--- crud/src/app/pages/employees/edit/edit.component.ts
+++ crud/src/app/pages/employees/edit/edit.component.ts
@@ -8,38 +8,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
-  employee: any = null;
-  employeeForm!: FormGroup;
-  private isEmail = /\S+@\S+\.\S+/;
-
-  constructor(private router: Router, private fb: FormBuilder) {
-    const navigation = this.router.getCurrentNavigation();
-    this.employee = navigation?.extras?.state?.['value'];
-    this.initForm();
-  }
-
-  ngOnInit(): void {
-     if (typeof this.employee === 'undefined') {
-       this.router.navigate(['new']);
-     } else {
-       this.employeeForm.patchValue(this.employee);
-     }
-  }
-
-  onSave(): void {
-    console.log('Saved', this.employeeForm.value); 
-  }
-
-  onGoBackToList(): void {
-    this.router.navigate(['list']);
-  }
-
-  private initForm(): void {
-    this.employeeForm = this.fb.group({
-      name: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.pattern(this.isEmail)]],
-      startDate: ['', [Validators.required]],
-    });
-  }
+  ngOnInit(): void {}
 }
