810c4dfbb1808dd2c2fc3e547edd90553da478c6
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-14 11:57:55 -0500

    using the logic that was in ediComponent for use only one form

--- crud/src/app/shared/components/employee-form/employee-form.component.ts
+++ crud/src/app/shared/components/employee-form/employee-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-form',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeFormComponent implements OnInit {
 
-  constructor() { }
+  employee: any = null;
+  employeeForm!: FormGroup;
+  private isEmail = /\S+@\S+\.\S+/;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private fb: FormBuilder) {
+    const navigation = this.router.getCurrentNavigation();
+    this.employee = navigation?.extras?.state?.['value'];
+    this.initForm();
   }
 
+  ngOnInit(): void {
+     if (typeof this.employee === 'undefined') {
+       this.router.navigate(['new']);
+     } else {
+       this.employeeForm.patchValue(this.employee);
+     }
+  }
+
+  onSave(): void {
+    console.log('Saved', this.employeeForm.value); 
+  }
+
+  onGoBackToList(): void {
+    this.router.navigate(['list']);
+  }
+
+  private initForm(): void {
+    this.employeeForm = this.fb.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(this.isEmail)]],
+      startDate: ['', [Validators.required]],
+    });
+  }
 }
