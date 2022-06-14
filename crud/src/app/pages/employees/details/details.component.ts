3ab95957bc2f750c5d61fd17ee4603d1c7d1188b
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-13 20:30:45 -0500

    adding on ngOnInit()

--- crud/src/app/pages/employees/details/details.component.ts
+++ crud/src/app/pages/employees/details/details.component.ts
@@ -7,20 +7,25 @@ import { NavigationExtras, Router } from '@angular/router';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+
+  employee: any = null;
+
   navigationExtras: NavigationExtras = {
     state: {
       value: null,
     },
   };
 
-  employee: any = null;
-
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     this.employee = navigation?.extras?.state?.['value'];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (typeof this.employee === 'undefined') {
+      this.router.navigate(['list']);
+    }
+  }
 
   onGoToEdit(): void {
     this.navigationExtras!.state!['value'] = this.employee;
