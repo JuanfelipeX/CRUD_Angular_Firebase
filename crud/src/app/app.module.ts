45d581e4b11066101df1c8efc4c3606e015bd1ce
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-14 12:12:42 -0500

    ng add @angular/fire

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HeaderModule } from './shared/components/header/header.module';
 import { EmployeeFormComponent } from './shared/components/employee-form/employee-form.component';
 import { EmployeeFormModule } from './shared/components/employee-form/employee-form.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { EmployeeFormModule } from './shared/components/employee-form/employee-f
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
-    EmployeeFormModule
+    EmployeeFormModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crud/src/environments/environment.ts
+++ crud/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crud-angular-employee-firebase',
+    appId: '1:911472001788:web:80def1896d86b4e4b30d79',
+    storageBucket: 'crud-angular-employee-firebase.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAI5SxsUtf-iLFpegvc3-Iy1fUQfLb5jrU',
+    authDomain: 'crud-angular-employee-firebase.firebaseapp.com',
+    messagingSenderId: '911472001788',
+    measurementId: 'G-QLH4KV7YDV',
+  },
   production: false,
 
   firebaseConfig: {
