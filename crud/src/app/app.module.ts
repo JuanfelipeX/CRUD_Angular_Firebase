06c6416fc0af298f04fd50f6af7c1776043d6d24
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-14 11:04:16 -0500

    ng g c shared/components/employee-form --module app

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HeaderModule } from './shared/components/header/header.module';
+import { EmployeeFormComponent } from './shared/components/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
