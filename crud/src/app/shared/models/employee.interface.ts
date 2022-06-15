2c00d7a4413f2cc230cc57f074a36175f281a0a4
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-15 11:52:45 -0500

    adding attributes to the interface Employee

--- crud/src/app/shared/models/employee.interface.ts
+++ crud/src/app/shared/models/employee.interface.ts
@@ -2,6 +2,9 @@ export interface Employee {
   id?: string;
   name: string;
   lastName: string;
+  age: Number;
+  job: String;
   email: string;
-  startDate: string;
+  startDate: Date;
+  cv: File;
 }
